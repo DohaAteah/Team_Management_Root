b7081ac5926d6447be1e72fdee0007cb0b624ab5
Yamen <103077478+yamenBayer@users.noreply.github.com>
2022-04-24 00:55:41 +0300

    Update_005

--- Team_Management/views.py
+++ Team_Management/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import Task
+from django.utils import timezone
 
 def Tasks_List(request):
-    return render(request , 'Home/Tasks_List.html' , {})
+    all_Tasks = Task.objects.filter(publish_Date__lte = timezone.now()).order_by('publish_Date')
+    return render(request , 'Home/Tasks_List.html' , {'all_Tasks': all_Tasks})
