95857b6bd6bcdc6b0e7120f346da9fc2fc12cec7
Yamen Bayer <yamen.warsss@gmail.com>
2022-05-04 15:28:01 +0300

    change_001

--- Team_Management/models.py
+++ Team_Management/models.py
@@ -5,8 +5,10 @@ from tkinter import CASCADE
 from turtle import title
 from django.db import models
 from django.conf import settings
+from django.forms import PasswordInput
 from django.utils import timezone
 from django.contrib.auth.models import User
+
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
