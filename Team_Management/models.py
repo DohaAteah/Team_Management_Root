cb2c6d3bce24d0674ada68ebc25ad0c3c4711438
Yamen Bayer <yamen.warsss@gmail.com>
2022-05-28 02:05:38 +0300

    tinker

--- Team_Management/models.py
+++ Team_Management/models.py
@@ -2,7 +2,6 @@ from datetime import date, datetime
 from email.policy import default
 from pydoc import describe
 from django.db.models.deletion import CASCADE
-from turtle import title
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
