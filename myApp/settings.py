cf776354e43b79f8b965a20a0b8ed39cc19c841b
Yamen Bayer <yamen.warsss@gmail.com>
2022-05-28 02:27:14 +0300

    update

--- myApp/settings.py
+++ myApp/settings.py
@@ -18,6 +18,7 @@ from . info import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ALLOWED_HOSTS = ['control2.herokuapp.com']
 
 EMAIL_USE_TLS = EMAIL_USE_TLS
 EMAIL_HOST = EMAIL_HOST
@@ -29,7 +30,8 @@ EMAIL_PORT = EMAIL_PORT
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bec95f8t6334ba$8=(!7endk&&aan!03rr0ewbttlhtt)qkz$4'
+#SECRET_KEY = 'django-insecure-bec95f8t6334ba$8=(!7endk&&aan!03rr0ewbttlhtt)qkz$4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
