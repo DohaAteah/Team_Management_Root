36e3ab0e8c6f90de5b6df5b28dc9e43fc1133a12
Yamen Bayer <yamen.warsss@gmail.com>
2022-05-28 12:45:17 +0300

    LastUpdate

--- myApp/settings.py
+++ myApp/settings.py
@@ -34,7 +34,7 @@ EMAIL_PORT = EMAIL_PORT
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'False')
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = []
 
