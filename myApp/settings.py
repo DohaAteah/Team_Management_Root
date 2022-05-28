3d16996128cd0a5df20bb0a8c5f53dd29b000347
Yamen Bayer <yamen.warsss@gmail.com>
2022-05-28 12:52:22 +0300

    LastUpdate

--- myApp/settings.py
+++ myApp/settings.py
@@ -34,7 +34,7 @@ EMAIL_PORT = EMAIL_PORT
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 CSRF_TRUSTED_ORIGINS = [
